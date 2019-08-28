e1ab04746e40d899194ecb776f6df80db6fa3305
Charlie Chiu <8595764@qq.com>
2019-08-28 12:21:05 +0800

    call gRPC method

---
+++ src/lib.js
@@ -0,0 +1,19 @@
+export let lib = {
+  //封装异步调用
+  asyncCall(client, funcName, ...args) {
+    return new Promise((resolve, reject) => {
+      let res = client[funcName](...args, function(err, response) {
+        if (err) {
+          reject(err);
+          return
+        }
+        resolve(response)
+      })
+    })
+  },
+
+  //统一定义grpc调用函数
+  async grpcCall(client, funcName, data, meta) {
+    return await this.asyncCall(client, funcName, data, meta)
+  },
+};
