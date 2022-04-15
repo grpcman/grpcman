4c39f36669477a48a428bf148a935cad74f96a6e
Jerry <calljerryli@outlook.com>
2022-04-16 02:12:40 +0800

    add @grpc/grpc-js & @grpc/proto-loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import './App.css'
+import {useState} from 'react'
 
 function App() {
+    const [f, setF] = useState<File>()
     return (
         <div>
             <h1>grpcman</h1>
             <p>🚀 正在施工中 🚀</p>
+            <input
+                type="file"
+                onChange={e => {
+                    if (e.target.files) {
+                        const f = e.target.files[0]
+                        console.log('f', f)
+                        setF(f)
+                    }
+                }}
+            />
         </div>
     )
 }
